2515cb4edb28762d1c0eeca3b27ee9e4b685997e
jaeltg <jaeltg@hotmail.com>
2021-04-10 20:27:44 +0100

    add text input filter

---
+++ src/components/QueenStats.vue
@@ -0,0 +1,26 @@
+<template>
+  <chart/>
+</template>
+
+<script>
+import {GChart} from 'vue-google-charts'
+import {eventBus} from '../main.js'
+
+export default {
+name: 'queen-stats',
+data () {
+    return {
+        chartOptions: {}
+    }
+},
+props: ['winData'],
+components: {
+    'chart': GChart
+},
+
+}
+</script>
+
+<style>
+
+</style>
